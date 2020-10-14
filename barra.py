d0e6f7347368263148194fc47b89e97ee847719b
Javcia98 <69157429+Javcia98@users.noreply.github.com>
2020-10-13 23:42:25 -0300

    Archivo barra para entrega 4
    Contiene la adaptacion a 3D  y las funciones:
    chequear_diseño(self, Fu, ϕ=0.9)
    obtener_factor_utilizacion(self, Fu, ϕ=0.9)
    rediseñar(self, Fu, ret, ϕ=0.9)

--- barra.py
+++ barra.py
@@ -1,90 +1,165 @@
 import numpy as np
-from numpy import  float64
+
 g = 9.81 #kg*m/s^2
 
+
 class Barra(object):
+
+    """Constructor para una barra"""
     def __init__(self, ni, nj, R, t, E, ρ, σy):
         super(Barra, self).__init__()
-        self.ni=ni
-        self.nj=nj
-        self.R=R
-        self.t=t
-        self.E=E
-        self.ρ=ρ
-        self.σy=σy
+        self.ni = ni
+        self.nj = nj
+        self.R = R
+        self.t = t
+        self.E = E
+        self.ρ = ρ
+        self.σy = σy
+
     def obtener_conectividad(self):
         return [self.ni, self.nj]
-    
+
     def calcular_area(self):
-        A=((np.pi)*(self.R **2))-np.pi*((self.R-self.t)**2) 
+        A = np.pi*(self.R**2) - np.pi*((self.R-self.t)**2)
         return A
 
     def calcular_largo(self, reticulado):
+        """Devuelve el largo de la barra. 
+        xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
+        xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
+        """
         xi = reticulado.obtener_coordenada_nodal(self.ni)
         xj = reticulado.obtener_coordenada_nodal(self.nj)
-        Largo = np.sqrt((xj[0]-xi[0])**2+(xj[1]-xi[1])**2+(xj[2]-xi[2])**2) 
-        return Largo
+        dij = xi-xj
+        return np.sqrt(np.dot(dij,dij))
 
     def calcular_peso(self, reticulado):
+        """Devuelve el largo de la barra. 
+        xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
+        xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
+        """
         L = self.calcular_largo(reticulado)
         A = self.calcular_area()
-        peso=L*A*g*self.ρ
-        return peso
-    
-    def obtener_rigidez(self, ret):
-        """Devuelve la rigidez ke del elemento. Arreglo numpy de (4x4)
-        ret: instancia de objeto tipo reticulado
-        """
-        L = self.calcular_largo(ret)
-        A = self.calcular_area()
-        K = self.E*A/L
         
+        return self.ρ * A * L * g
+
+
+
+    def obtener_rigidez(self, ret):
+        A = self.calcular_area()
+        L = self.calcular_largo(ret)
+
         xi = ret.obtener_coordenada_nodal(self.ni)
         xj = ret.obtener_coordenada_nodal(self.nj)
-        dij =(xi-xj)
-        cos = dij[0]/L
-        sen = dij[1]/L
-        T0 = np.array([[-cos], [-sen], [cos], [sen]])
-        
-        ke = (T0 @ T0.T) * K
-        
 
-        return ke 
+        cosθx = (xj[0] - xi[0])/L
+        cosθy = (xj[1] - xi[1])/L
+        cosθz = (xj[2] - xi[2])/L
+
+        Tθ = np.array([ -cosθx, -cosθy, -cosθz, cosθx, cosθy, cosθz]).reshape((6,1))
+
+        return self.E * A / L * (Tθ @ Tθ.T )
 
     def obtener_vector_de_cargas(self, ret):
-        """Devuelve el vector de cargas nodales fe del elemento. Vector numpy de (4x1)
-        ret: instancia de objeto tipo reticulado
-        """
         W = self.calcular_peso(ret)
-        vec = np.array([0, -1, 0, -1])
-        fe = vec.T * W/2
-    
-        return fe
+
+        return np.array([0, 0, -W, 0, 0, -W])
 
 
     def obtener_fuerza(self, ret):
-        """Devuelve la fuerza se que debe resistir la barra. Un escalar tipo double. 
-        ret: instancia de objeto tipo reticulado
-        """
-        L = self.calcular_largo(ret)
+        ue = np.zeros(6)
+        ue[0:3] = ret.obtener_desplazamiento_nodal(self.ni)
+        ue[3:] = ret.obtener_desplazamiento_nodal(self.nj)
+        
         A = self.calcular_area()
-        K = self.E*A/L
+        L = self.calcular_largo(ret)
+
+        xi = ret.obtener_coordenada_nodal(self.ni)
+        xj = ret.obtener_coordenada_nodal(self.nj)
+
+        cosθx = (xj[0] - xi[0])/L
+        cosθy = (xj[1] - xi[1])/L
+        cosθz = (xj[2] - xi[2])/L
+
+        Tθ = np.array([ -cosθx, -cosθy, -cosθz, cosθx, cosθy, cosθz]).reshape((6,1))
+
+        return self.E * A / L * (Tθ.T @ ue)
+
+
+
+
+
+    def chequear_diseño(self, Fu,ret, ϕ=0.9):
+        A = self.calcular_area()
+        Fn = A*self.σy
         
-        n1 = self.ni
-        n2 = self.nj
+        if ϕ*Fn < abs(Fu):
+            return False
+        else:
+            return True
+
+
+    def obtener_factor_utilizacion(self, Fu, ϕ=0.9):
+        """Para la fuerza Fu (proveniente de una combinacion de cargas)
+        calcular y devolver el factor de utilización
+        """
+        A = self.calcular_area()
+        Fn = A*self.σy   
+        FU = abs(Fu) / (ϕ*Fn) 
         
-        di = ret.obtener_coordenada_nodal(self.ni)
-        dj = ret.obtener_coordenada_nodal(self.nj)
-        dij =(di-dj)
-        cos = dij[0]/L
-        sen = dij[1]/L
-        T0 = np.array([[-cos], [-sen], [cos], [sen]])
+        return FU
+
+
+    def rediseñar(self, Fu, ret, ϕ=0.9):
+        """Para la fuerza Fu (proveniente de una combinacion de cargas)
+        re-calcular el radio y el espesor de la barra de modo que
+        se cumplan las disposiciones de diseño lo más cerca posible
+        a FU = 1.0.
+        """
+        FU = self.obtener_factor_utilizacion(Fu, ϕ)
+        A = self.calcular_area()
+        Fn = A*self.σy
         
-        Kreal = T0.T * K
+        def inercia(self):
+            I = 3.141592*((self.R**4)-(self.R-self.t)**4)/4
+            return I
         
-       
-        ue =np.array([ret.u[2*n1], ret.u[2*n1 + 1], ret.u[2*n2], ret.u[2*n2 + 1]])
+        L = self.calcular_largo(ret)
+        I = inercia(self)
+        i =(I/A)**(1/2)
+        landa = L/i
         
-        se = Kreal@ue
-        np.float64(se)
-        return -se 
+        while (FU <= 0.95) or (FU >= 1) or (Fn*ϕ < Fu) or (landa > 300):
+            
+            if (FU <= 0.95):
+                self.R = 0.9*self.R #self.R += -1*cm   
+                self.t = 0.9*self.t # self.t += -1*mm
+        
+            if (FU >= 1) or (Fn*ϕ < Fu):
+                self.R = 1.1*self.R #self.R += 1*cm   
+                self.t = 1.1*self.t #self.t += 1*mm
+                
+            I = inercia(self)
+            i = (I/A)**(1/2)
+            landa = L/i    
+            FU = self.obtener_factor_utilizacion(Fu, ϕ)    
+            A = self.calcular_area()
+            Fn = A*self.σy
+            
+            if (landa > 300):
+                self.R = 1.1*self.R #self.R += 1*cm
+        
+        
+        
+        return 
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
